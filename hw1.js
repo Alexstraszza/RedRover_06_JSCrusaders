3a9783caabf5cca382102d8404de462fbafc549a
Alexsandra Astapenka <aostapenko66@gmail.com>
2023-04-08 02:20:54 +0400

    test push

---
+++ hw1.js
@@ -0,0 +1,17 @@
+// 1.1 Напишите программу по согласованию отпуска в зависимости от месяца. Если месяц “July” или “August”, то программа должна показать - ‘approved’, если другие месяцы, то - “denied”.
+
+// function Months(Jan, Feb, March, Apr, May, June, July, August, Sept, Oct, Nov, Dec) {
+//     if (July == true && August == true)
+//         console.log ("approved");
+    
+//     else console.log("denied")
+// }
+
+let month = "Dec"
+
+if (month == "July" || month == "August") {
+    console.log("approved") 
+}
+else {
+    console.log("denied")
+}
